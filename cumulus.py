d20d4ea5f4c2061b22c0902704f3ec8568a638be
Nick Bartley <github@nb221.com>
2014-10-15 20:53:07 +0100

    Debugging #5

--- cumulus.py
+++ cumulus.py
@@ -253,16 +253,16 @@ class Cloud(object):
 		
 		return levels
 
-		def list_devices(self):
-			# List all audio input devices
-			p = pyaudio.PyAudio()
-			i = 0
-			n = p.get_device_count()
-			while i < n:
-				dev = p.get_device_info_by_index(i)
-				if dev['maxInputChannels'] > 0:
-					print str(i)+'. '+dev['name']
-				i += 1
+	def list_devices(self):
+		# List all audio input devices
+		p = pyaudio.PyAudio()
+		i = 0
+		n = p.get_device_count()
+		while i < n:
+			dev = p.get_device_info_by_index(i)
+			if dev['maxInputChannels'] > 0:
+				print str(i)+'. '+dev['name']
+			i += 1
 		
 		
 if __name__ == "__main__":
