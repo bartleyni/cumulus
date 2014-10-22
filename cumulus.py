5d9a92725905106f6622709bd70b032347f0ea07
Nick Bartley <github@nb221.com>
2014-10-22 18:36:42 +0100

    Updated pygame mixer.init()

--- cumulus.py
+++ cumulus.py
@@ -32,7 +32,7 @@ class Cloud(object):
 	#This is the main cloud loop		
 	def run(self):
 		while True:
-			#pygame.mixer.init()
+			pygame.mixer.init()
 			if self.PIFACE.input_pins[0].value == 1:
 				self.alloff()
 				time.sleep(1)
@@ -198,11 +198,11 @@ class Cloud(object):
 	def thunder(self):
 		silence = self.PIFACE.input_pins[3].value
 		self.randomSound = random.randint(1,4)
-		pygame.mixer.init()
+		#pygame.mixer.init()
 		if silence == 0:
 			effect = pygame.mixer.Sound("/home/pi/cumulus/thunder/"+str(self.randomSound)+".wav")
 			effect.play()
-		pygame.mixer.quit()
+		#pygame.mixer.quit()
 
 	def audio_playback(self):
 		# Set up audio
